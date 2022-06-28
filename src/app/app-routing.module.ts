b09170af922165cb67179dc6e3cac541024a3d45
carlos benavides <1992cab@gmail.com>
2022-06-27 23:07:50 -0500

    chore: added default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
     path: 'users',
     loadChildren: () => import(`./modules/users/users.module`).then(m => m.UsersModule),
   },
+  { path: '', pathMatch: 'full', redirectTo: 'users' },
 ];
 
 @NgModule({
